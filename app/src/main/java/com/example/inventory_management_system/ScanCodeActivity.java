420ca1819111681c5d67d9dfa9025e07a999e4a2
Rahul <rahulismt491@gmail.com>
2021-05-01 03:46:29 +0545

    Inventory Management

--- app/src/main/java/com/example/inventory_management_system/ScanCodeActivity.java
+++ app/src/main/java/com/example/inventory_management_system/ScanCodeActivity.java
@@ -1,14 +1,54 @@
 package com.example.inventory_management_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class ScanCodeActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int My_PERMISSIONS_REQUEST_CAMERA =0;
+    ZXingScannerView ScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_code);
+        ScannerView=new ZXingScannerView(this);
+        setContentView(ScannerView);
+    }
+
+    @Override
+    public void handleResult(Result Result) {
+        Add_Item.resulttextview.setText(Result.getText());
+        onBackPressed();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ScannerView.stopCamera();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    My_PERMISSIONS_REQUEST_CAMERA);
+
+        }
+        ScannerView.setResultHandler(this);
+        ScannerView.startCamera();
     }
 }
