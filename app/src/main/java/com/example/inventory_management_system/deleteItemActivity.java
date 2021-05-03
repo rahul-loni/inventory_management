b9bfe0f24be9b5cdf8636df799c7ce92c630fb5e
Rahul <rahulismt491@gmail.com>
2021-05-03 08:05:46 +0545

    Inventory Management

---
+++ app/src/main/java/com/example/inventory_management_system/deleteItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.inventory_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class deleteItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_item);
+    }
+}
---
+++ app/src/main/java/com/example/inventory_management_system/scan_items.java
@@ -0,0 +1,14 @@
+package com.example.inventory_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scan_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_items);
+    }
+}
